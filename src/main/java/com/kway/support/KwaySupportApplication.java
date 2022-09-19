cc2a3f525aee2493df56be3bb98213f7a2a5b4f9
venh <conf312@naver.com>
2022-09-19 18:30:40 +0900

    [modify] RequestMapping

--- src/main/java/com/kway/support/KwaySupportApplication.java
+++ src/main/java/com/kway/support/KwaySupportApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class KwaySupportApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(KwaySupportApplication.class, args);
 	}
 
--- src/main/java/com/kway/support/web/InquiryController.java
+++ src/main/java/com/kway/support/web/InquiryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/support")
+@RequestMapping("/inquiry")
 @RequiredArgsConstructor
 @RestController
 public class InquiryController {
